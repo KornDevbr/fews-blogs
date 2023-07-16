61e3e5eddc1980c9ae4fd7b6d271518f77715980
Yevhen Korniievskyi <y.korniievskyi@dev-branch.com>
2023-07-16 14:54:50 +0300

    Added backgtound image. Configured about_us page to use css styles. Styled about us page using style.css file.

--- code/about_us.php
+++ code/about_us.php
@@ -1,19 +1,32 @@
-<?php include("user_panel.php")?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us | Fews Blogs</title>
+    <!-- <link href="styles/reset.css" rel="stylesheet" /> -->
+    <link href="styles/style.css" rel="stylesheet" />
+    <!-- <link
+        href="https://fonts.googleapis.com/css?family=Open+sans"
+        rel="stylesheet" /> -->
 </head>
+<header>
+    <?php include("user_panel.php")?>
+</header>
 <body>
     <h1 align="center">Fews Blogs</h1>
     <h2 align="center">About us</h2>
-    <p align="center">We are a little website for blogging. We contain your thoughts about everything.</br>
+    <p class="paragraph" align="center">
+        We are the little website for blogging.</br>
+        We contain your thoughts about everything.</br>
         Don't wait, share what you think about everything.</br>
         Read what everyone thinks about everything.</br>
-        Leave comments about everything under everyone's article about everything.</br>
+        Leave comments about everything under </br>
+        everyone's article about everything.</br>
         <b>Be</b> like everyone, <b>writ</b>e about ev<b>er</b>ything.</p></br>
     <p align="center">Back to <a href="index.php">Homepage</a></p>
+<footer>
+</footer>
 </body>
 </html>
