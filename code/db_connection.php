b33e6f5fdec7147903c60ae94b032ac8e0d4ae92
Yevhen Korniievskyi <y.korniievskyi@dev-branch.com>
2023-11-13 23:14:27 +0200

    Self-Study: Created functions for prepared MySQL statements executing. Edited the article.php file to use prepared MySQL statements, prettified the file. Prettified the db_connection.php file, added reporting mode to it.

--- code/db_connection.php
+++ code/db_connection.php
@@ -1,11 +1,14 @@
 <?php
-    // Database connection.
-    $db_host    = "mariadb";
-    $db_name    = "fews-blogs";
-    $db_user    = "fews-blogs";
-    $db_password = "fews-blogs";
-    $db_connection = mysqli_connect($db_host, $db_name, $db_user, $db_password);
-    // Check connection.
-    if (mysqli_connect_errno()){
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
+// Database connection.
+$db_host    = "mariadb";
+$db_name    = "fews-blogs";
+$db_user    = "fews-blogs";
+$db_password = "fews-blogs";
+$db_connection = mysqli_connect($db_host, $db_name, $db_user, $db_password);
+// Check connection.
+if (mysqli_connect_errno()){
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+// Set mysqli error reporting mode.
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
---
+++ code/mysql_secure_query.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * Returns an array with prepared secure SELECT SQL query.
+ * @param mysqli_stmt $stmt Prepared MySQL query.
+ * @param array $params An array with referenced variables for SQL query.
+ * For example: array(&$variable1, &$variable2, ... , &$variableN).
+ * @return array
+ */
+function secureMysqliQuerySelect(mysqli_stmt $stmt,array $params):array{
+
+    // Count input params and make a string with the type of input.
+    $type = '';
+    for ($i = 1; $i <= count($params); $i++) {
+        $type .= "s";
+    }
+
+    // Create an array from two other arrays to pass it in function.
+    $bindParams = array_merge(array($stmt, $type), $params);
+
+    // Call the mysqli_stmt_bind_param function with the parameters stored in the $bindParams array.
+    call_user_func_array('mysqli_stmt_bind_param', $bindParams);
+    mysqli_stmt_execute($stmt);
+
+    $result = mysqli_stmt_get_result($stmt);
+
+    return mysqli_fetch_assoc($result);
+}
+
+/** The function makes the prepared MySQLi query secure.
+ * @param mysqli_stmt $stmt Prepared MySQL query.
+ * @param array $params An array with referenced variables for SQL query.
+ *  For example: array(&$variable1, &$variable2, ... , &$variableN).
+ * @return bool
+ */
+function secureMysqliQueryInsert(mysqli_stmt $stmt, array $params):bool{
+
+    // Count input params and make a string with the type of input.
+    $type = '';
+    for ($i = 1; $i <= count($params); $i++) {
+        $type .= "s";
+    }
+
+    // Create an array from two other arrays to pass it in function.
+    $bindParams = array_merge(array($stmt, $type), $params);
+
+    // Call the mysqli_stmt_bind_param function with the parameters stored in the $bindParams array.
+    call_user_func_array('mysqli_stmt_bind_param', $bindParams);
+
+    return mysqli_stmt_execute($stmt);
+}
+
+/** The function makes prepared array with MySQL results for using with loops.
+ * @param mysqli_stmt $stmt Prepared MySQL query.
+ * @param array|null $params An array with data to pass to the MySQL query.
+ * @return array
+ */
+function secureMysqliQuerySelectForLoop(mysqli_stmt $stmt, array $params = null):array {
+
+    // Count input params and make a string with the type of input.
+    $paramTypes = '';
+    for ($i = 1; $i <= count($params); $i++) {
+        $paramTypes .= "s";
+    }
+
+    // Bind parameters if provided.
+    if ($paramTypes && $params) {
+        // Create a temporary array of references for parameters.
+        $bindParams = array($stmt, $paramTypes);
+        foreach ($params as &$param) {
+            $bindParams[] = &$param;
+        }
+
+        // Call mysqli_stmt_bind_param with the temporary array.
+        call_user_func_array('mysqli_stmt_bind_param', $bindParams);
+    }
+
+    // Execute the statement.
+    if (mysqli_stmt_execute($stmt)) {
+        // Get result set metadata
+        $resultMetadata = mysqli_stmt_result_metadata($stmt);
+
+        // Fetch field names from metadata.
+        $fieldNames = array();
+        while ($field = mysqli_fetch_field($resultMetadata)) {
+            $fieldNames[] = $field->name;
+        }
+
+        // Create an array to hold result column references.
+        $resultColumns = array_fill_keys($fieldNames, null); // Initialize with null.
+
+        // Bind the result columns to references.
+        $bindResultParams = array($stmt);
+        foreach ($resultColumns as &$value) {
+            $bindResultParams[] = &$value;
+        }
+        call_user_func_array('mysqli_stmt_bind_result', $bindResultParams);
+
+        // Fetch and return the results.
+        $results = array();
+        while (mysqli_stmt_fetch($stmt)) {
+            $row = array();
+            foreach ($resultColumns as $key => $value) {
+                $row[$key] = $value;
+            }
+            $results[] = $row;
+        }
+
+        return $results;
+    } else {
+        die("Error executing statement: " . mysqli_stmt_error($stmt));
+    }
+}
